392eb90264c96e997d16041b5d66c148d055176b
bryanwhyte <3875675+bryanwhyte@users.noreply.github.com>
2020-09-02 19:03:33 -0400

    Create stage_tests.sh

---
+++ stage_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash +x
+echo ' '
+echo '[INFO] ------------------------------------------------------------------------'
+echo '[INFO]  '
+echo '[INFO] execute mvn-stage tests :' simple-app-${BUILD_ID}
+sleep 2s
+echo '[INFO]  .'
+sleep 2s
+echo '[INFO]  ..'
+sleep 2s
+echo '[INFO]  ...'
+sleep 2s
+echo '[INFO]  ....'
+sleep 2s
+echo '[INFO]  .....'
+sleep 2s
+echo '[INFO]  ......'
+sleep 2s
+echo '[INFO]  .......'
+sleep 2s
+echo '[INFO]  ........'
+sleep 2s
+echo '[INFO]  .........'
+sleep 2s
+echo '[INFO]  ..........'
+sleep 2s
+echo '[INFO]  ...........'
+sleep 2s
+echo '[INFO]  ............'
+sleep 2s
+echo '[INFO]  .............'
+sleep 2s
+echo '[INFO]  ..............'
+sleep 2s
+echo '[INFO]  ...............'
+echo '[INFO] mvn-stage PASSED - move :' simple-app-${BUILD_ID} 'to mvn-release'
+echo '[INFO]  '
+echo '[INFO] ------------------------------------------------------------------------'
