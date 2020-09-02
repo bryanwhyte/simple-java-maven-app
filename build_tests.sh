738d944f2549846526d271e18818617c0a57f8ea
bryanwhyte <3875675+bryanwhyte@users.noreply.github.com>
2020-09-02 17:48:15 -0400

    Create build_tests.sh

---
+++ build_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash +x
+echo ' '
+echo '[INFO] ------------------------------------------------------------------------'
+echo '[INFO]  '
+echo '[INFO] execute mvn-build tests :' simple-app-${BUILD_ID}
+sleep 2s
+echo '[INFO]  .'
+sleep 2s
+echo '[INFO]  ..'
+sleep 2s
+echo '[INFO]  ...'
+sleep 2s
+echo '[INFO]  ....'
+sleep 2s
+echo '[INFO]  .....'
+sleep 2s
+echo '[INFO]  ......'
+sleep 2s
+echo '[INFO]  .......'
+sleep 2s
+echo '[INFO]  ........'
+sleep 2s
+echo '[INFO]  .........'
+sleep 2s
+echo '[INFO]  ..........'
+sleep 2s
+echo '[INFO]  ...........'
+sleep 2s
+echo '[INFO]  ............'
+sleep 2s
+echo '[INFO]  .............'
+sleep 2s
+echo '[INFO]  ..............'
+sleep 2s
+echo '[INFO]  ...............'
+echo '[INFO] mvn-build PASSED - move :' simple-app-${BUILD_ID} 'to mvn-stage'
+echo '[INFO]  '
+echo '[INFO] ------------------------------------------------------------------------'
